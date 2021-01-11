cda9977b0708e01ebb032bc1856979d4242481e4
Rodrigo Calabretta <Rodrigo_Calabretta@neimanmarcus.com>
2021-01-11 00:26:20 -0300

    Add main funtionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import Header from './componentes/Header'
 import Formulario from './componentes/Formulario'
-import { Fragment } from 'react';
+import Mensaje from './componentes/Mensaje'
+import Resultado from './componentes/Resultado'
+import { Fragment , useState } from 'react';
+import Spinner from './componentes/Spinner';
 
 function App() {
+  //Definir state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [plazo, guardarPlazo] = useState('');
+  const [total, guardarTotal] = useState(0);
+  const [cargando, guardarCargando] = useState(false)
+
+  let componente;
+  
+  if(cargando===true){
+    componente = <Spinner/>
+
+  } else if(total === 0){
+    componente = <Mensaje/>
+    
+  }else{
+    componente = <Resultado
+                plazo={plazo}
+                cantidad={cantidad}
+                total={total}
+                />
+  }
+
   return (
     <Fragment>
       <Header 
@@ -10,9 +35,21 @@ function App() {
       />
       
 
-      <div className='container'>
-      <Formulario/>
+    <div className='container'>
+      <Formulario
+        cantidad={cantidad}
+        guardarCantidad={guardarCantidad}
+        plazo={plazo}
+        guardarPlazo={guardarPlazo}
+        guardarTotal={guardarTotal}
+        guardarCargando={guardarCargando}
+        />
+
+      <div className= "mensajes">
+        {componente}
       </div>
+      
+    </div>
 
     </Fragment>
   );
--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Fragment, useState } from 'react';
+import {calcularTotal} from '../helpers';
 
-const Formulario = () => {
+const Formulario = (props) => {
+
+    const {cantidad, guardarCantidad,plazo, guardarPlazo, guardarTotal, guardarCargando} = props;
+
+    //Definir state
+    const [error, guardarError]= useState(false)
+
+    //Cuando el usuario hace un submit
+    const calcularPrestamo = e =>{
+        e.preventDefault()
+
+        //Validar
+        if( cantidad===0 || plazo===''){
+            guardarError(true)
+            return;
+        }
+
+        //Eliminar el error previo
+        guardarError(false)
+
+        //Habilitar spiner
+        guardarCargando(true)
+
+        setTimeout(() => {
+            //Realizar cotizacion
+            const total = calcularTotal(cantidad, plazo)
+       
+            //Una vez calculado, guardar total
+            guardarTotal(total)
+            
+            //Deshabilitar spiner
+            guardarCargando(false)
+        }, 3000);
+        
+    }
     return ( 
-        <form>
+        <Fragment>
+        <form onSubmit={calcularPrestamo}>
+            
             <div className="row">
                 <div>
                     <label>Cantidad Prestamo</label>
@@ -10,12 +48,14 @@ const Formulario = () => {
                         className="u-full-width" 
                         type="number" 
                         placeholder="Ejemplo: 3000" 
+                        onChange={ e => guardarCantidad(parseInt(e.target.value))}
                     />
                 </div>
                 <div>
                     <label>Plazo para Pagar</label>
                     <select 
                         className="u-full-width"
+                        onChange={ e => guardarPlazo(parseInt(e.target.value))}
                     >
                         <option value="">Seleccionar</option>
                         <option value="3">3 meses</option>
@@ -33,6 +73,10 @@ const Formulario = () => {
                 </div>
             </div>
         </form>
+
+        {(error) ? <p className="error">Todos los campos son obligatorios</p> : null}
+        
+        </Fragment>
      );
 }
  
